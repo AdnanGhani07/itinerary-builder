2b7fdd40c5538839319d9f997c5e66ad6a97df4b
Adnan Ghani <agadnanrocks07@gmail.com>
2025-10-17 20:15:01 +0530

    Refactored Changes

--- src/components/VisaDetailsInput.tsx
+++ src/components/VisaDetailsInput.tsx
@@ -35,18 +35,21 @@ export const VisaDetailsInput: React.FC<VisaDetailsInputProps> = ({ value, onCha
       <h3 className="mb-2 font-bold">Visa Details</h3>
       <div className="flex gap-2 mb-2 items-center">
         <input
+          name="visaType"
           className="border px-2"
           placeholder="Visa Type"
           value={value.visaType}
           onChange={e => handleChange(e)}
         />
         <input
+          name="validity"
           className="border px-2"
           placeholder="Validity"
           value={value.validity}
           onChange={e => handleChange(e)}
         />
         <input
+          name="processingDate"
           className="border px-2"
           type="string"
           placeholder="Processing Date"
